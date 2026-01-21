6c168fe73b26495582195a327d5a9009834fc3ea
Poorna Prakash <p211@exeter.ac,in>
2026-01-21 11:00:51 +0000

    added the comments to understand how to push

--- hello.py
+++ hello.py
@@ -5,4 +5,7 @@ if __name__ == "__main__":
     hello_world()
 
 #create a new rep in github. copy the repo path. come to antigravity and click on clone repo. 
-#
+#paste the path. add a new file and add a code to check (if needed)
+#go to source control on the left and add the commit and save and commit it
+#go the three dots above and click push and then click on sync
+#go to github and refresh to see the file
